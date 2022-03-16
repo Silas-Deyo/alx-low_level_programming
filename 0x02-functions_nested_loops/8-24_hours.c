72ce732f07bb8450bb27bb8fcea825bcbe4003d4
Silas <ayomideadeyo01@gmail.com>
2022-03-16 09:09:47 -0700

    function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
  *
  * Return: void
  */
@@ -9,6 +9,7 @@
 void jack_bauer(void)
 {
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+
 	hours_max = 58;
 	hours_tens = '0';
 
